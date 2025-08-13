2ebd1f559a7fc3b53bb74a2663f6bc33927139ce
skymkmk <i@skymkmk.com>
2025-08-13 10:40:15 +0800

    style: adjust copyright claims to fit smaller device

--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,18 @@
-// Domain List to SRS, a converter that transforms domain-list-community rules into SRS.
+// Domain List to SRS, a converter that transforms domain-list-community rules
+// into SRS.
 // Copyright (C) 2025 skymkmk
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-// You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License version 3
+// as published by the Free Software Foundation.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use std::{collections::BTreeSet, ops::AddAssign};
 
